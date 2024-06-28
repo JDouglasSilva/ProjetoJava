38adf41d7cfc86ec7a818ad0c15e52a81b51eace
Dag <jddouglassilva@gmail.com>
2024-06-28 18:44:18 -0300

    Att tradu

--- front/src/assets/i18n/es.js
+++ front/src/assets/i18n/es.js
@@ -1,27 +1,32 @@
 export const ES = {
-    login: {
-      email: "Correo electrónico",
-      password: "Contraseña",
-      submit: "Iniciar sesión",
-      createAccount: "Crear una cuenta",
-      error: "Credenciales inválidas. Por favor, inténtelo de nuevo."
-    },
-    register: {
-      username: "Nombre de usuario",
-      email: "Correo electrónico",
-      password: "Contraseña",
-      submit: "Registrarse",
-      usernameExists: "El nombre de usuario ya existe. Por favor, inténtelo de nuevo.",
-      emailExists: "El correo electrónico ya existe. Por favor, inténtelo de nuevo.",
-      fieldRequired: "El campo {field} es obligatorio."
-    },
-    home: {
-      welcome: "Bienvenido, {username}",
-      logout: "Cerrar sesión"
-    },
-    navbar: {
-      home: "Inicio",
-      users: "Usuarios"
+  login: {
+    email: "Correo electrónico",
+    password: "Contraseña",
+    submit: "Iniciar sesión",
+    createAccount: "Crear cuenta",
+    error: "Credenciales inválidas. Por favor, inténtelo de nuevo."
+  },
+  register: {
+    username: "Nombre de usuario",
+    email: "Correo electrónico",
+    password: "Contraseña",
+    submit: "Registrarse",
+    usernameExists: "Nombre de usuario ya existe. Por favor, inténtelo de nuevo.",
+    emailExists: "Correo electrónico ya existe. Por favor, inténtelo de nuevo.",
+    fieldRequired: "El campo {field} es obligatorio."
+  },
+  home: {
+    welcome: "Bienvenido, {username}",
+    logout: "Cerrar sesión",
+    functionalities: {
+      urlShortener: "Acortador de URL",
+      virtualPantry: "Despensa Virtual"
     }
-  };
-  
+  },
+  navbar: {
+    home: "Inicio",
+    users: "Usuarios",
+    profile: "Editar Perfil",
+    logout: "Cerrar sesión"
+  }
+};
--- front/src/assets/i18n/pt.js
+++ front/src/assets/i18n/pt.js
@@ -1,27 +1,32 @@
 export const PT = {
-    login: {
-      email: "Email",
-      password: "Senha",
-      submit: "Entrar",
-      createAccount: "Criar Conta",
-      error: "Credenciais inválidas. Por favor, tente novamente."
-    },
-    register: {
-      username: "Nome de usuário",
-      email: "Email",
-      password: "Senha",
-      submit: "Registrar",
-      usernameExists: "Nome de usuário já existe. Por favor, tente novamente.",
-      emailExists: "Email já existe. Por favor, tente novamente.",
-      fieldRequired: "Campo {field} está em branco."
-    },
-    home: {
-      welcome: "Bem-vindo, {username}",
-      logout: "Sair"
-    },
-    navbar: {
-      home: "Início",
-      users: "Usuários"
+  login: {
+    email: "Email",
+    password: "Senha",
+    submit: "Entrar",
+    createAccount: "Criar Conta",
+    error: "Credenciais inválidas. Por favor, tente novamente."
+  },
+  register: {
+    username: "Nome de usuário",
+    email: "Email",
+    password: "Senha",
+    submit: "Registrar",
+    usernameExists: "Nome de usuário já existe. Por favor, tente novamente.",
+    emailExists: "Email já existe. Por favor, tente novamente.",
+    fieldRequired: "Campo {field} é obrigatório."
+  },
+  home: {
+    welcome: "Bem-vindo, {username}",
+    logout: "Sair",
+    functionalities: {
+      urlShortener: "Encurtador de URL",
+      virtualPantry: "Despensa Virtual"
     }
-  };
-  
+  },
+  navbar: {
+    home: "Início",
+    users: "Usuários",
+    profile: "Editar Perfil",
+    logout: "Sair"
+  }
+};
